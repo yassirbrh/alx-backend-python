86aa548df7e2577d2a099a0291f046f8de694b9d
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-01-11 17:44:37 +0000

    Defining and annotating some variables with a specified values.

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+    Defining and annotating the following variables with the specified values:
+    @a:                         integer, value=1
+    @pi:                        float,   value=3.14
+    @i_understand_annotations:  boolean, value=True
+    @school:                    string,  value='Holberton'
+'''
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
