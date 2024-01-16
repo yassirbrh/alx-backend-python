614b29f68683e7d26ed908b1606d608f46c484a5
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-01-16 15:49:27 +0000

    Coroutine called async_generator that takes no arguments and loops 10 times, waiting 1 second and then yield a random number from 0 to 10.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,9 +6,10 @@
 '''
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     '''
         async_generator: function
         return: yield a random number between 0 and 10.
