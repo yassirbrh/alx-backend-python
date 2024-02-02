464475c08c40fa16324f159505e3f72c1f26ee14
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-02-02 19:19:55 +0000

    Class TestAccessNestedMap that inherits from unittest.TestCase, and adding the test for the function access_nested_map.

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+'''
+    TestAccessNestedMap class that inherits from unittest.TestCase.
+'''
+from parameterized import parameterized
+import unittest
+
+
+access_nested_map = __import__('utils').access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    '''
+        Class that inherits from unittest.TestCase to test access_nested_map.
+    '''
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, output):
+        '''
+            test_access_nested_map: function
+            @self: class constructor.
+            @nested_map: nested map to get the values from it.
+            @path: the path to follow to get the output.
+            @output: the output to get.
+            return: no return
+        '''
+        self.assertEqual(access_nested_map(nested_map, path), output)
+
+
+if __name__ == '__main__':
+    unittest.main()
