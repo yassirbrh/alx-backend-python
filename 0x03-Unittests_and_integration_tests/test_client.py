e655fcabcaabb70a3f942c51a138697ee3ce3d74
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-02-04 16:35:44 +0000

    Add the Class TestGithubOrgClient that inherits from unittest.TestCase, and the test_org method to test the method org from GithubOrgClient.

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+'''
+    Class TestGithubOrgClient that inherits from unittest.TestCase
+'''
+from client import GithubOrgClient
+from parameterized import parameterized
+from typing import Dict
+import unittest
+from unittest.mock import Mock, MagicMock, patch
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    '''
+        Class TestGithubOrgClient that inherits from unittest.TestCase.
+    '''
+    @parameterized.expand([
+        ("google", {"login": "google"}),
+        ("abc", {"login": None})
+        ])
+    @patch('client.get_json')
+    def test_org(self, org: str, response: Dict, mock_mtd: MagicMock) -> None:
+        '''
+            Test method for GithubOrgClient.org
+        '''
+        mock_mtd.return_value = MagicMock(return_value=response)
+        github_org_client = GithubOrgClient(org)
+        self.assertEqual(github_org_client.org(), response)
+        new_org = "https://api.github.com/orgs/{}".format(org)
+        mock_mtd.assert_called_once_with(new_org)
