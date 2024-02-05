765ea3d3f22575b4a635c0653a18b53333bdd59c
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-02-05 18:19:46 +0000

    Adding the test method test_has_license to test has_license.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -62,7 +62,7 @@ class TestGithubOrgClient(unittest.TestCase):
             return: No return.
         '''
         test_payload = {
-            'repos_url': 'https://api.github.com/users/google/repos',
+            'repos_url': "https://api.github.com/users/google/repos",
             'repos': [
                 {
                     "id": 460600860,
@@ -95,3 +95,19 @@ class TestGithubOrgClient(unittest.TestCase):
             )
             mocked_public_repos_url.assert_called_once()
         mocked_get_json.assert_called_once()
+
+    @parameterized.expand([
+        ({'license': {'key': "bsd-3-clause"}}, "bsd-3-clause", True),
+        ({'license': {'key': "bsl-1.0"}}, "bsd-3-clause", False),
+    ])
+    def test_has_license(self, repo: Dict, key: str, result: bool) -> None:
+        """
+            test_has_licence: function
+            @self: class constructor.
+            @repo: repository.
+            @key: key of licence.
+            @result: True if it has, otherwise False
+        """
+        github_org_client = GithubOrgClient("google")
+        client_has_licence = github_org_client.has_license(repo, key)
+        self.assertEqual(client_has_licence, result)
